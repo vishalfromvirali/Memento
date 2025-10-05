38f7062e0fe2aaa47256f19be3c81bcd4a798dbe
vishalfromvirali <vishalfromvirali@gmail.com>
2025-10-05 14:41:31 +0530

    hello

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 app=Flask(__name__)
 app.config['SCHEDULER_API_ENABLED'] = True 
 app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT']=587
+app.config['MAIL_PORT']=587   
 app.config['MAIL_USE_TLS']=True
 app.config['MAIL_USERNAME']='bb4106402@gmail.com'
 app.config['MAIL_PASSWORD']='qjrhwiwnscjlsvbi'
